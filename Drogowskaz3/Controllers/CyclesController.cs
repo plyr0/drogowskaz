e75a0e7eac4fb7e5a1df205731abcc9a59a18b53
Mateusz Teklak <plyr00@gmail.com>
2017-09-11 20:23:33 +0200

    CyclesController

---
+++ Drogowskaz3/Controllers/CyclesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class CyclesController : Controller
+    {
+        private drogowskazEntities db = new drogowskazEntities();
+
+        // GET: Cycles
+        public ActionResult Index()
+        {
+            return View(db.Cycles.ToList());
+        }
+
+        // GET: Cycles/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cycle cycle = db.Cycles.Find(id);
+            if (cycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cycle);
+        }
+
+        // GET: Cycles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cycles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Cycle cycle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cycles.Add(cycle);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cycle);
+        }
+
+        // GET: Cycles/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cycle cycle = db.Cycles.Find(id);
+            if (cycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cycle);
+        }
+
+        // POST: Cycles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Cycle cycle)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cycle).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cycle);
+        }
+
+        // GET: Cycles/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cycle cycle = db.Cycles.Find(id);
+            if (cycle == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cycle);
+        }
+
+        // POST: Cycles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Cycle cycle = db.Cycles.Find(id);
+            db.Cycles.Remove(cycle);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
