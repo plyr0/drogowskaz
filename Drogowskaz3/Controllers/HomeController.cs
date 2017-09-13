698fe149bd881a1605790d1b17a5726288fd3d5b
s426243 <s426243@PC14I>
2017-09-13 11:44:42 +0200

    Test

--- Drogowskaz3/Controllers/HomeController.cs
+++ Drogowskaz3/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using DrogowskazSerwer.Function;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Functions;
 
 namespace WebApplication1.Controllers
 {
@@ -10,6 +12,18 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
+            DateTime start;
+            DateTime end;
+            GenerateCycle.Adwent(2017,out start, out end);
+            ViewBag.Swieta = "Adwent przypada w: " + start.ToString() + " do " + end.ToString();
+            GenerateCycle.OkresBozonarodzeniowy(2017, out start, out end);
+            ViewBag.Swieta += "</br> Okres Bożonarodzeniowy przypada w: " + start.ToString() + " do " + end.ToString();
+            GenerateCycle.OkresZmartwychwstaniaPanskiego(2017, out start, out end);
+            ViewBag.Swieta += "</br> Okres Zmartwychwstania Pańskiego przypada w: " + start.ToString() + " do " + end.ToString();
+            GenerateCycle.TriduumPaschalne(2017, out start, out end);
+            ViewBag.Swieta += "</br> Triduum Paschalne przypada w: " + start.ToString() + " do " + end.ToString();
+            GenerateCycle.WielkiPost(2017, out start, out end);
+            ViewBag.Swieta += "</br> Wielki Post przypada w: " + start.ToString() + " do " + end.ToString();
             return View();
         }
 
