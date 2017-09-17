5cc76d4f6437ff1b404e0c768aa2d074a5461a66
Mateusz Teklak <plyr00@gmail.com>
2017-09-17 16:31:39 +0200

    datetimepicker

--- Drogowskaz3/App_Start/BundleConfig.cs
+++ Drogowskaz3/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace WebApplication1
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/moment-with-locales.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap-datetimepicker.min.js",
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datetimepicker.min.css",
                       "~/Content/site.css"));
         }
     }
